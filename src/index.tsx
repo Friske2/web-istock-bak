d258d1c85ad2ed45f9bcec44024641580b9daef1
jellopy <parnupat.j@gmail.com>
2022-03-21 21:53:35 +0700

    init redux

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,14 @@ import "antd/dist/antd.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from "./reducers/store";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,23 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../reducers/CouterSlice";
+import { Button } from "antd";
+interface StateCounter {
+  counter: {
+    value: number;
+  };
+}
 function Home() {
-  return <div>Home</div>;
+  const count = useSelector((state: StateCounter) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Home</h1>
+      count : {count}
+      <Button onClick={() => dispatch(increment())}>+</Button>
+      <Button onClick={() => dispatch(decrement())}>-</Button>
+    </div>
+  );
 }
 
 export default Home;
